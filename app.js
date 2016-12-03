1ebadc37972ceb61545db7a02c7f2110764aa5eb
nokogiring <beefupme@gmail.com>
2016-12-03 16:59:31 +0900

    ajax

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 let express = require("express");
 let app = new express;
 
+
+// サーバーの起動
 var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-// app.set('view engine', 'html');
-
-// app.get("/", function(req, res, next){
-//     res.render("index", {});
-// });
-
+// publicフォルダ以下の静的ファイルを公開
 app.use(express.static('public'));
+
+var webpages = {
+    "google":"https://www.google.co.jp/",
+    "amazon":"https://www.amazon.co.jp/",
+    "apple":"http://www.apple.com/jp/"
+}; 
+
+// API
+// "/api/webpages"のURLにgetするとJSON形式のwebpagesオブジェクトを返却する
+app.get("/api/webpages", function(req, res){
+    res.json(webpages);
+});
